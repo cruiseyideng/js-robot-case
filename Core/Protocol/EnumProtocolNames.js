fbc8b8ebbfd5fad8d197839094fe6f0484ef042e
cruise <cruiseyideng@gmail.com>
2019-12-21 14:18:50 +0700

    Add: protocolnames

---
+++ Core/Protocol/EnumProtocolNames.js
@@ -0,0 +1,40 @@
+// this file is automatic generated!!!!
+// don't manually modify!!!!
+
+// 协议命名空间
+
+const EnumProtocolNames = {
+    CustomServer:  "CustomServer",
+    Erba:  "Erba",
+    GameFactory:  "GameFactory",
+    Baccarat:  "Baccarat",
+    SXNiuNiu:  "SXNiuNiu",
+    Dispatch:  "Dispatch",
+    SXLunPan:  "SXLunPan",
+    HongBao:  "HongBao",
+    ffc:  "ffc",
+    WebGame:  "WebGame",
+    Ddz:  "Ddz",
+    BankerBull:  "BankerBull",
+    three:  "three",
+    SXLongHu:  "SXLongHu",
+    FFCBaiJiaLe:  "FFCBaiJiaLe",
+    Godofwealth:  "Godofwealth",
+    DaXiaoDanShuang:  "DaXiaoDanShuang",
+    FongShen:  "FongShen",
+    RedBlack:  "RedBlack",
+    SXBaiJiaLe:  "SXBaiJiaLe",
+    FFCLongHu:  "FFCLongHu",
+    Lobby:  "Lobby",
+    FishFactory:  "FishFactory",
+    FishDispatch:  "FishDispatch",
+    LongHu:  "LongHu",
+    Flower:  "Flower",
+    Fish:  "Fish",
+    ThreeKingDoms:  "ThreeKingDoms",
+    BonusBears:  "BonusBears",
+    Login:  "Login",
+    GodofwealthNew:  "GodofwealthNew",
+}
+
+module.exports = EnumProtocolNames
